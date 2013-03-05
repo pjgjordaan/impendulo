7776acc1391d47d23bd7a437fae757a59d5a0c34
Pieter Jordaan <pjgjordaan@gmail.com>
2013-03-05 18:15:04 +0200

    Server works for multitple clients

--- client/client.go
+++ client/client.go
@@ -4,17 +4,12 @@ import( "net")
 
 type Client struct {
 	Token byte
-	Incoming chan string
-	Outgoing chan string
 	Conn net.Conn
-	Quit chan bool
 	File string
-	data []byte
-	offset int
 }
 
-func NewClient(tok byte, out chan string, con net.Conn, fname string)(*Client){
-	return &Client{tok, make(chan string), out, con, make(chan bool), fname, make([]byte, 4096), 0}
+func NewClient(tok byte, con net.Conn, fname string)(*Client){
+	return &Client{tok, con, fname}
 }
 
 func (c *Client) Equal(other *Client) bool {
@@ -27,21 +22,5 @@ func (c *Client) Equal(other *Client) bool {
 }
 
 func (c *Client) Close() {
-	c.Quit <- true
 	c.Conn.Close()
-}
-func (c *Client) AddData(data []byte){
-	copied := 0
-	if c.offset < len(c.data){
-		copied = copy(c.data[c.offset:],data)
-		c.offset += len(data)
-	}
-	if(copied < len(data)){
-		c.data = append(c.data, data[copied:]...)
-		c.offset = len(c.data)
-	}
-}
-
-func (c *Client) GetData()([]byte){
-	return c.data[:c.offset]
 }
