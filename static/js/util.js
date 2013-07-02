b19d95f6b09bc46eb8244edb13ec501826c23d61
godfried <pjgjordaan@gmail.com>
2013-07-02 15:42:42 +0200

    extracted javascript

---
+++ static/js/util.js
@@ -0,0 +1,19 @@
+function movevalue (srcParent, destParent, src) {
+    var dest = document.createElement("option");
+    dest.innerHTML = src.value;
+    dest.setAttribute("value", src.value);
+    dest.setAttribute("onclick", "movevalue('"+destParent+"', '"+srcParent+"', this)");
+    document.getElementById(destParent).appendChild(dest);
+    document.getElementById(srcParent).removeChild(src);
+}
+
+function unhide (it, box) {
+    console.log(it);
+    var check = (box.checked) ? "block" : "none";
+    document.getElementById(it).style.display = check;
+}
+
+window.onload = function () {
+    dp.SyntaxHighlighter.ClipboardSwf = 'static/flash/clipboard.swf';
+    dp.SyntaxHighlighter.HighlightAll('code');
+}
