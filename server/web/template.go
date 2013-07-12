1716d8a1e6129c2f2f528f0b758853451d381043
godfried <pjgjordaan@gmail.com>
2013-07-12 13:49:28 +0200

    moved templates to static folder

--- server/web/template.go
+++ server/web/template.go
@@ -24,14 +24,15 @@ func setBreaks(val string) template.HTML {
 	return template.HTML(strings.Replace(val, "\n", "<br>", -1))
 }
 
-var basicT = []string{filepath.Join("templates", "_base.html"), filepath.Join("templates", "index.html"), filepath.Join("templates", "messages.html")}
+var dir = filepath.Join("static", "templates")
+var basicT = []string{filepath.Join(dir, "_base.html"), filepath.Join(dir, "index.html"), filepath.Join(dir, "messages.html")}
 
 func T(names ...string) *template.Template {
 	t := template.New("_base.html").Funcs(funcs)
 	all := make([]string, len(basicT)+len(names))
 	end := copy(all, basicT)
 	for i, name := range names {
-		all[i+end] = filepath.Join("templates", name)
+		all[i+end] = filepath.Join(dir, name)
 	}
 	t = template.Must(t.ParseFiles(all...))
 	return t
