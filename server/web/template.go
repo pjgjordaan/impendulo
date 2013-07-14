8253831254784a0844e5a5a8d7158d887ff040e9
godfried <pjgjordaan@gmail.com>
2013-07-14 22:05:16 +0200

    added diffs

--- server/web/template.go
+++ server/web/template.go
@@ -6,6 +6,8 @@ import (
 	"html/template"
 	"path/filepath"
 	"strings"
+	"github.com/sergi/go-diff/diffmatchpatch"
+	"bytes"
 )
 
 var funcs = template.FuncMap{
@@ -16,6 +18,37 @@ var funcs = template.FuncMap{
 	"address":     address,
 	"base": filepath.Base,
 	"shortname": shortname,
+	"isCode": isCode,
+	"diff": diff,
+}
+
+func diff(orig, change string)template.HTML{
+	
+	dmp := diffmatchpatch.New()
+	diffs := dmp.DiffMain(orig, change, true)
+	buffer := new(bytes.Buffer)
+	for _, diff := range diffs{
+		var class, symbol string
+		switch diff.Type{
+		case diffmatchpatch.DiffDelete:
+			class = "text-error"
+			symbol = "--"
+		case diffmatchpatch.DiffInsert:
+			class = "text-success"
+			symbol = "++"
+		case diffmatchpatch.DiffEqual:
+			continue
+		}
+		buffer.WriteString(`<p class="`+class+`">`+symbol)
+		buffer.WriteString(_setBreaks(diff.Text))
+		buffer.WriteString(`</p><br>`)
+			
+	}
+	return template.HTML(buffer.String())
+}
+
+func isCode(name string)bool{
+	return strings.ToLower(name) == "code"
 }
 
 func shortname(exec string) string {
@@ -32,7 +65,11 @@ func address(val interface{}) string {
 }
 
 func setBreaks(val string) template.HTML {
-	return template.HTML(strings.Replace(val, "\n", "<br>", -1))
+	return template.HTML(_setBreaks(val))
+}
+
+func _setBreaks(val string) string {
+	return strings.Replace(val, "\n", "<br>", -1)
 }
 
 var dir = filepath.Join("static", "templates")
---
+++ tool/sissy/sissyResult.go
@@ -0,0 +1,83 @@
+package checkstyle
+
+import (
+	"labix.org/v2/mgo/bson"
+	"reflect"
+	"github.com/godfried/impendulo/util"
+	"encoding/xml"
+	"html/template"
+)
+
+const NAME = "Checkstyle"
+
+type CheckstyleResult struct {
+	Id     bson.ObjectId "_id"
+	FileId bson.ObjectId "fileid"
+	Time   int64         "time"
+	Data   *CheckstyleReport    "data"
+}
+
+func (this *CheckstyleResult) Equals(that *CheckstyleResult) bool {
+	return reflect.DeepEqual(this, that)
+}
+
+func (this *CheckstyleResult) Name() string{
+	return NAME
+}
+
+func (this *CheckstyleResult) GetId() bson.ObjectId{
+	return this.Id
+}
+
+func (this *CheckstyleResult) GetFileId() bson.ObjectId{
+	return this.FileId
+}
+
+func (this *CheckstyleResult) String() string {
+	return "Type: tool.java.CheckstyleResult; Id: "+this.Id.Hex()+"; FileId: "+this.FileId.Hex() + "; Time: "+ util.Date(this.Time)
+}
+
+func (this *CheckstyleResult) TemplateArgs(current bool)(string, interface{}){
+	if current{
+		return "checkstyleCurrent.html", this.Data
+	}else{
+		return "checkstyleNext.html", this.Data
+	}
+}
+
+func (this *CheckstyleResult) Success() bool{
+	return true
+}
+
+
+func NewResult(fileId bson.ObjectId, data []byte) (res *CheckstyleResult, err error){
+	res = &CheckstyleResult{Id: bson.NewObjectId(), FileId: fileId, Time: util.CurMilis()}
+	res.Data, err = genReport(res.Id, data)
+	return 
+}
+
+func genReport(id bson.ObjectId, data []byte) (res *CheckstyleReport, err error) {
+	if err = xml.Unmarshal(data, &res); err != nil {
+		return
+	}
+	res.Id = id
+	return
+}
+
+type CheckstyleReport struct{
+	Id bson.ObjectId 
+	Version string `xml:"version,attr"`
+	Files []*File `xml:"file"`
+}
+type File struct{
+	Name string `xml:"name,attr"`
+	Errors []*Error `xml:"error"`
+}
+
+type Error struct{
+	Line int `xml:"line,attr"`
+	Column int `xml:"column,attr"`
+	Severity string `xml:"severity,attr"`
+	Message template.HTML `xml:"message,attr"`
+	Source string `xml:"source,attr"`
+}
